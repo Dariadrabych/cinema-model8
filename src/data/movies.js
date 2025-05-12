413e63737637e65bfce0f66676e0b8beb20b3a9c
Dariadrabych bc2024-6 <dariadaiii06@gmail.com>
2025-05-12 16:08:59 +0630

    feat: додано масив фільмів у movies.js

---
+++ src/data/movies.js
@@ -0,0 +1,23 @@
+const movies = [
+  {
+    id: 1,
+    title: "Інтерстеллар",
+    description: "Наукова фантастика про подорожі у часі та просторі.",
+    genre: "Фантастика",
+    poster: "https://www.kinogallery.com/images/interstellar/kinogallery.com-interstellar-552104.jpg",
+    date: "2025-05-15",
+    time: "19:00"
+  },
+  {
+    id: 2,
+    title: "Темний лицар",
+    description: "Бетмен проти Джокера у кримінальному Ґотемі.",
+    genre: "Екшн",
+    poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    date: "2025-05-16",
+    time: "20:30"
+  }
+];
+
+
+export default movies;
