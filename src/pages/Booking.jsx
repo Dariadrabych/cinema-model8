2e4de6defb3dac306ec028759cf66020c03c86d2
Dariadrabych bc2024-6 <dariadaiii06@gmail.com>
2025-05-25 15:24:47 +0630

    feat: реалізовано компонент Booking для вибору місць

---
+++ src/pages/Booking.jsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom';
+import CinemaHall from '../components/CinemaHall';
+
+const Booking = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Бронювання фільму #{id}</h1>
+      <CinemaHall />
+    </div>
+  );
+};
+
+export default Booking;
